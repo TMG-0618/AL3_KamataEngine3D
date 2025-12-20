587897fe21b30992540ba51dba2dc3f2f05f8289
Midzuki_T <k025g0043@g.neec.ac.jp>
2025-12-21 00:26:26 +0900

    masterを初期状態にリセット

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -5,33 +5,20 @@ using namespace KamataEngine;
 GameScene::GameScene() { Initialize(); }
 
 GameScene::~GameScene() { 
-	delete sprite_;
+
 }
 
 void GameScene::Initialize() {
-	textureHandle_ = TextureManager::Load("MHbrk.jpg");
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-	model_ = Model::Create();
+
 }
 
 void GameScene::Update() {
 
-	Vector2 position = sprite_->GetPosition();
-
-	position.x += 2.0f;
-	position.y += 1.0f;
-
-	sprite_->SetPosition(position);
 
 }
 
 void GameScene::Draw() {
 
-	Sprite::PreDraw();
-
-	sprite_->Draw();
-
-	Sprite::PostDraw();
 
 }
 
