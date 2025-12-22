82f70332ad420f940428697ac85f9b9330edd4ee
Midzuki_T <k025g0043@g.neec.ac.jp>
2025-12-22 23:14:24 +0900

    Player.hを1_1から持ってきた

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,2 +1,20 @@
-#pragma once
-class Player {};
+#pragma once #pragma once
+#include "KamataEngine.h"
+
+class Player {
+
+private:
+	KamataEngine::WorldTransform worldTransform_;
+	KamataEngine::Model* model_ = nullptr;
+	KamataEngine::Camera* camera_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
+
+public:
+	Player();
+	~Player();
+
+	void Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera);
+	void Update();
+	void Draw();
+};
