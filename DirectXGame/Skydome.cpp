b7a3fda07c8994e54324bf2c75c39fbe6bb959f6
Midzuki_T <k025g0043@g.neec.ac.jp>
2025-12-22 22:22:35 +0900

    cppコピー

--- DirectXGame/Skydome.cpp
+++ DirectXGame/Skydome.cpp
@@ -1,1 +1,25 @@
 #include "Skydome.h"
+
+using namespace KamataEngine;
+
+Skydome::Skydome() {}
+
+Skydome::~Skydome() {}
+
+void Skydome::Initialize(Model* model, const WorldTransform& worldTransform, Camera* camera) {
+	model_ = model;
+
+	worldTransform_.scale_ = worldTransform.scale_;
+	worldTransform_.rotation_ = worldTransform.rotation_;
+	worldTransform_.translation_ = worldTransform.translation_;
+	worldTransform_.matWorld_ = worldTransform.matWorld_;
+	worldTransform_.parent_ = worldTransform.parent_;
+
+	camera_ = new Camera();
+
+	camera_ = camera;
+}
+
+void Skydome::Update() {}
+
+void Skydome::Draw() {}
