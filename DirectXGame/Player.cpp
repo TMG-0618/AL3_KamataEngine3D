7880bda56cbb40fc4fc40abcad640d31489f8439
Midzuki_T <k025g0043@g.neec.ac.jp>
2025-12-22 23:17:03 +0900

    player.cppを持ってきた

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,1 +1,28 @@
 #include "Player.h"
+#include <assert.h>
+
+using namespace KamataEngine;
+
+Player::Player() {}
+
+Player::~Player() {}
+
+void Player::Initialize(Model* model, uint32_t textureHandle, Camera* camera) {
+
+	assert(model);
+
+	model_ = model;
+	textureHandle_ = textureHandle;
+	camera_ = camera;
+	worldTransform_.Initialize();
+}
+
+void Player::Update() { worldTransform_.TransferMatrix(); }
+
+void Player::Draw() {
+	model_->PreDraw();
+
+	model_->Draw(worldTransform_, *camera_, textureHandle_);
+
+	model_->PostDraw();
+}
