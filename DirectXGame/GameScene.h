1d671a6f1d5505e17dd201b84eec6d961753fb47
Midzuki_T <k025g0043@g.neec.ac.jp>
2025-12-22 22:23:51 +0900

    gameScene.hコピー

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "KamataEngine.h"
+#include "Skydome.h"
+#include <memory>
 #include <vector>
 
 class GameScene {
 
 private:
 	KamataEngine::Model* model_ = nullptr;
-	KamataEngine::Camera camera_;
+	KamataEngine::Camera* camera_;
 	std::vector<std::vector<KamataEngine::WorldTransform*>> worldTransformBlocks_;
 	KamataEngine::DebugCamera* debugCamera_ = nullptr;
 	bool isDebugCameraActive_ = false;
+
+	std::unique_ptr<Skydome> skydome_;
+	KamataEngine::Model* modelSkydome_ = nullptr;
+	KamataEngine::WorldTransform worldTransformSkydome_;
 
 public:
 	GameScene();
