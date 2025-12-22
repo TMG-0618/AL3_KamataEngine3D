faffbf5001fe57c9cca5788f17fe426ca0b3d1d2
Midzuki_T <k025g0043@g.neec.ac.jp>
2025-12-22 15:58:47 +0900

    5_1終わり

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,27 +2,31 @@
 
 using namespace KamataEngine;
 
-GameScene::GameScene() { Initialize(); }
+GameScene::GameScene() { 
+	Initialize();
+}
 
-GameScene::~GameScene() { 
-
+GameScene::~GameScene() {
+	delete model_;
+	delete player_;
 }
 
 void GameScene::Initialize() {
 
 	grHandle_ = TextureManager::Load("./Resources/MHbrk.jpg");
+	model_ = Model::Create();
+	camera_ = new Camera();
+	camera_->Initialize();
 
+	player_ = new Player();
+
+	player_->Initialize(model_,grHandle_,camera_);
 }
 
 void GameScene::Update() {
-
-
-
+	player_->Update();
 }
 
-void GameScene::Draw() {
-
-
-
+void GameScene::Draw() { 
+	player_->Draw(); 
 }
-
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "KamataEngine.h"
+#include "Player.h"
 
 class GameScene {
 
 private:
 
-	uint32_t grHandle_ = -1;
+	uint32_t grHandle_ = 0;
+	KamataEngine::Model* model_ = nullptr;
+	KamataEngine::Camera* camera_ = nullptr;
 
+	Player* player_ = nullptr;
 
 public:
 	GameScene();
